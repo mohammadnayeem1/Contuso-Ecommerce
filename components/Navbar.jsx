444bee206d49a4eb292364647906e0f1e9aef17f
mohammadnayeem1 <mohammadnayeem2000@gmail.com>
2022-08-25 22:05:32 -0400

    added cart functionality

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,16 +6,24 @@ import { Cart } from './';
 import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Contuso</Link>
+        <Link href="/">Constuso</Link>
       </p>
 
-      <button type="button" className="cart-icon">
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
         <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   );
 };
