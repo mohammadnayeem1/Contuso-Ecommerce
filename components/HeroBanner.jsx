cbb272a07f03432f725ec162aa3efd5598ec2f7f
mohammadnayeem1 <mohammadnayeem2000@gmail.com>
2022-08-26 23:25:07 -0400

    Added cart, stripe functionality, success page, troubleshooting

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -18,7 +18,7 @@ const HeroBanner = ({ heroBanner }) => {
         />
 
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
+          <Link href={`/product/${heroBanner.slug.current}`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
